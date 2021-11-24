68fb6714c461c07f0186ded64ea9618f0d055462
ehsan2793 <ehsan2793@gmail.com>
2021-11-24 04:56:13 -0800

    header componet completed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import Login from './components/Login'
 import './App.css'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Header from './components/Header'
+import Home from './components/Home'
 
 function App() {
   return (
@@ -8,7 +10,11 @@ function App() {
       <Router>
         <Switch>
           <Route exact path='/'>
-            <Login/>
+            <Login />
+          </Route>
+          <Route path='/home'>
+            <Header />
+            <Home />
           </Route>
         </Switch>
       </Router>
---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+import React from 'react'
+
+const Home = (props) => {
+    return (
+        <div>
+            home
+        </div>
+    )
+}
+
+export default Home
