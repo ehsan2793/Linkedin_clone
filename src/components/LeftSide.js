0b41d4cd8215f137290c79d072f8409d697aedef
ehsan2793 <ehsan2793@gmail.com>
2021-11-26 02:01:18 -0800

    small changes debug

--- src/components/LeftSide.js
+++ src/components/LeftSide.js
@@ -5,9 +5,9 @@ const Leftside = (props) => {
         <Container>
             <ArtCard>
                 <UserInfo>
-                    <CardBackground/>
+                    <CardBackground />
                     <a>
-                        <Photo/>
+                        <Photo />
                         <Link>Welcome, there!</Link>
                     </a>
                     <a>
@@ -20,14 +20,14 @@ const Leftside = (props) => {
                             <span>Connections</span>
                             <span>Grow your network</span>
                         </div>
-                        <img src="/images/widget-icon.svg" alt=""/>
+                        <img src="/images/widget-icon.svg" alt="" />
                     </a>
                 </Widget>
                 <Item>
-                    <span>
-                        <img src="/images/item-icon.svg" alt=""/>
-                        My Items
-                    </span>
+          <span>
+            <img src="/images/item-icon.svg" alt="" />
+            My Items
+          </span>
                 </Item>
             </ArtCard>
 
@@ -36,10 +36,10 @@ const Leftside = (props) => {
                     <span>Groups</span>
                 </a>
                 <a>
-                    <span>
-                        Events
-                        <img src="/images/plus-icon.svg" alt=""/>
-                    </span>
+          <span>
+            Events
+            <img src="/images/plus-icon.svg" alt="" />
+          </span>
                 </a>
                 <a>
                     <span>Follow Hashtags</span>
@@ -118,38 +118,31 @@ const Widget = styled.div`
   border-bottom: 1px solid rgba(0, 0, 0, 0.15);
   padding-top: 12px;
   padding-bottom: 12px;
-
   & > a {
     text-decoration: none;
     display: flex;
     justify-content: space-between;
     align-items: center;
     padding: 4px 12px;
-
     &:hover {
       background-color: rgba(0, 0, 0, 0.08);
     }
-
     div {
       display: flex;
       flex-direction: column;
       text-align: left;
-
       span {
         font-size: 12px;
         line-height: 1.333;
-
         &:first-child {
           color: rgba(0, 0, 0, 0.6);
         }
-
         &:nth-child(2) {
           color: rgba(0, 0, 0, 1);
         }
       }
     }
   }
-
   svg {
     color: rgba(0, 0, 0, 1);
   }
@@ -161,17 +154,14 @@ const Item = styled.a`
   padding: 12px;
   font-size: 12px;
   display: block;
-
   span {
     display: flex;
     align-items: center;
     color: rgba(0, 0, 0, 1);
-
     svg {
       color: rgba(0, 0, 0, 0.6);
     }
   }
-
   &:hover {
     background-color: rgba(0, 0, 0, 0.08);
   }
@@ -182,28 +172,23 @@ const CommunityCard = styled(ArtCard)`
   text-align: left;
   display: flex;
   flex-direction: column;
-
   a {
     color: black;
     padding: 4px 12px 4px 12px;
     font-size: 12px;
-
     &:hover {
       color: #0a66c2;
     }
-
     span {
       display: flex;
       align-items: center;
       justify-content: space-between;
     }
-
     &:last-child {
       color: rgba(0, 0, 0, 0.6);
       text-decoration: none;
       border-top: 1px solid #d6cec2;
       padding: 12px;
-
       &:hover {
         background-color: rgba(0, 0, 0, 0.08);
       }
