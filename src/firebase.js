c0cd694be8c52d6623d824f85255fdeaeaf8fca9
ehsan2793 <ehsan2793@gmail.com>
2021-11-27 21:12:55 -0800

    firebase set up completed

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDCMRHiAbd_s8wsYXfBqRRVhg0oQWhWY2M",
+    authDomain: "linkedin-e.firebaseapp.com",
+    projectId: "linkedin-e",
+    storageBucket: "linkedin-e.appspot.com",
+    messagingSenderId: "256420864532",
+    appId: "1:256420864532:web:2bcf512d4d774de36b3d01",
+    measurementId: "G-JX35HW27GK"
+};
+
+const firebaseApp = firebase.initialize(firebaseConfig)
+const db = firebaseApp.firestore()
+const auth = firebaseApp.auth()
+const provider = new firebase.auth.GoogleAuthProvider()
+const storage = firebase.storage()
+
+export {auth ,provider, storage};
+export default db
