176d3d166a453ddaa075dd879bafa9ebb22837c8
ehsan2793 <ehsan2793@gmail.com>
2021-11-27 17:13:10 -0800

    more dependencies

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components'
 import React from 'react'
+import {connect} from "react-redux";
 
 const Login = (props) => {
     return (
@@ -166,4 +167,11 @@ const Google = styled.button`
     color: rgba(0, 0, 0, 0.75);
   }
 `
-export default Login
+const mapStateToProps = (state) => {
+    return {
+
+    }
+}
+
+const mapDispatchToProps = (dispatch) => ({})
+export default connect(mapStateToProps,mapDispatchToProps)(Login)
