add589a6b5f1d102882d1333318bbe7080114985
ehsan2793 <ehsan2793@gmail.com>
2021-11-27 16:12:30 -0800

    store added

---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+import {combineReducers} from "redux";
+import userReducer from "./useReducer.js";
+const rootReducer = combineReducers({
+    userState: userReducer
+})
+
+
+export default rootReducer
